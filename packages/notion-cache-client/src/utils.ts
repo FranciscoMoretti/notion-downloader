0c61da1df56bfb95ada37d9f07849a537ef0a0b9
Francisco Moretti <franciscoemoretti@gmail.com>
2024-07-23 13:17:37 +0100

    cleanup deps

--- packages/notion-cache-client/src/utils.ts
+++ packages/notion-cache-client/src/utils.ts
@@ -1,14 +1,5 @@
 import fs from "fs-extra"
 
-export function convertToUUID(str: string): string {
-  if (str.length !== 32) {
-    throw new Error("Input string must be 32 characters long")
-  }
-  return `${str.slice(0, 8)}-${str.slice(8, 12)}-${str.slice(
-    12,
-    16
-  )}-${str.slice(16, 20)}-${str.slice(20)}`
-}
 export async function saveDataToJson(data: any, filename: string) {
   const json = JSON.stringify(data, null, 2)
   await fs.writeFile(filename, json)
