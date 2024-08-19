19e53878f22d239ec51043390792e8439b7aea43
Francisco Moretti <franciscoemoretti@gmail.com>
2024-08-19 09:50:19 +0100

    new fixtures

--- packages/notion-cache-client/test/fixtures/sample-site/downloader.config.cjs
+++ packages/notion-cache-client/test/fixtures/sample-site/downloader.config.cjs
@@ -4,8 +4,7 @@ module.exports = {
   rootObjectType: "page",
   rootId: "74fe3069cc484ee5b94fb76bd67732ae",
   cache: {
-    cleanCache: false,
-    cacheStrategy: "cache",
+    cleanCache: true,
   },
   skipConversion: true,
 }
