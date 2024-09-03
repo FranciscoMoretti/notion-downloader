735776226027b510d43eb37e42f5d0efc8dc26d8
Francisco Moretti <franciscoemoretti@gmail.com>
2024-09-03 19:26:45 +0100

    make tree traversal more generic

--- packages/download-notion/src/getFileTreeMap.ts
+++ packages/download-notion/src/getFileTreeMap.ts
@@ -5,6 +5,7 @@ import { LayoutStrategy } from "./LayoutStrategy"
 import { NotionDatabase } from "./NotionDatabase"
 import { NotionPage } from "./NotionPage"
 import { NotionObjectsData } from "./objects_utils"
+import { traverseTree } from "./traverseTree"
 
 export async function getFileTreeMap(
   currentPath: string,
@@ -18,63 +19,36 @@ export async function getFileTreeMap(
     // TODO: Handle block objects and make notionObject never undefined
     return
   }
-  const notionObject = getNotionObject(
+  const nodeAction = (
+    notionObject: NotionDatabase | NotionPage,
+    parentContext: {
+      path: string
+      databaseIsRoot: boolean
+    }
+  ) => {
+    const newLevelPath = !parentContext.databaseIsRoot
+      ? layoutStrategy.newLevel(parentContext.path, notionObject)
+      : parentContext.path
+    const objectPath =
+      notionObject.object == "database"
+        ? newLevelPath
+        : layoutStrategy.getPathForPage(notionObject, parentContext.path)
+    filesManager.set("base", notionObject.object, notionObject.id, {
+      path: objectPath,
+      lastEditedTime: notionObject.lastEditedTime,
+    })
+    return {
+      path: newLevelPath,
+      databaseIsRoot: false,
+    }
+  }
+  traverseTree(
+    {
+      path: currentPath,
+      databaseIsRoot: databaseIsRootLevel,
+    },
+    objectsTreeNode,
     objectsData,
-    objectsTreeNode.id,
-    objectsTreeNode.object
+    nodeAction
   )
-
-  // Non-traversal logic start
-  const newLevelPath = !databaseIsRootLevel
-    ? layoutStrategy.newLevel(currentPath, notionObject)
-    : currentPath
-  const objectPath =
-    notionObject.object == "database"
-      ? newLevelPath
-      : layoutStrategy.getPathForPage(notionObject, currentPath)
-  filesManager.set("base", notionObject.object, objectsTreeNode.id, {
-    path: objectPath,
-    lastEditedTime: notionObject.lastEditedTime,
-  })
-  // Non-traversal logic end
-
-  for (const childObject of objectsTreeNode.children) {
-    await getFileTreeMap(
-      newLevelPath,
-      childObject,
-      objectsData,
-      false,
-      layoutStrategy,
-      filesManager
-    )
-  }
-}
-
-export function getNotionObject(
-  objectData: NotionObjectsData,
-  currentID: string,
-  type: "page" | "database"
-) {
-  if (type === "page") {
-    return getNotionPage(objectData, currentID)
-  } else if (type === "database") {
-    return getNotionDatabase(objectData, currentID)
-  } else {
-    throw new Error(`Unknown object type: ${type}`)
-  }
-}
-
-export function getNotionPage(
-  objectsData: NotionObjectsData,
-  currentID: string
-) {
-  const pageResponse = objectsData.page[currentID]
-  return new NotionPage(pageResponse)
-}
-export function getNotionDatabase(
-  objectsData: NotionObjectsData,
-  currentID: string
-) {
-  const databaseResponse = objectsData.database[currentID]
-  return new NotionDatabase(databaseResponse)
 }
