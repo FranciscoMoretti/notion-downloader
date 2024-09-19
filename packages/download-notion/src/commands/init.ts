f5a5d0b6e7548a9b27897c072c43e02d28dfa408
Francisco Moretti <franciscoemoretti@gmail.com>
2024-09-19 20:14:01 +0100

    implement init command

--- packages/download-notion/src/commands/init.ts
+++ packages/download-notion/src/commands/init.ts
@@ -1,11 +1,7 @@
 import { existsSync, promises as fs } from "fs"
 import path from "path"
 import {
-  DEFAULT_COMPONENTS,
   DEFAULT_CONFIG_FILENAME,
-  DEFAULT_TAILWIND_CONFIG,
-  DEFAULT_TAILWIND_CSS,
-  DEFAULT_UTILS,
   configSchema,
   getConfig,
   resolveConfigPaths,
@@ -13,21 +9,14 @@ import {
 } from "@/src/utils_old/get-config"
 import { handleError } from "@/src/utils_old/handle-error"
 import { logger } from "@/src/utils_old/logger"
-import * as templates from "@/src/utils_old/templates"
 import chalk from "chalk"
 import { Command } from "commander"
-import template from "lodash.template"
 import { cacheStrategiesSchema } from "notion-downloader"
 import ora from "ora"
 import prompts from "prompts"
 import { z } from "zod"
 
-import {
-  defaultOptionsSchema,
-  defaultPullOptions,
-  pullOptionsSchema,
-  rootObjectTypeSchema,
-} from "../config/schema"
+import { defaultPullOptions, rootObjectTypeSchema } from "../config/schema"
 
 const initOptionsSchema = z.object({
   cwd: z.string(),
@@ -62,31 +51,8 @@ export const init = new Command()
           `Configuration files already exist in directory ${cwd}. This operation will have no effect.`
         )
       } else {
-        // Read config.
         const config = await promptForConfig(cwd, options.yes)
-        // Write to file. (move to init)
-
-        if (!options.yes) {
-          const { proceed } = await prompts({
-            type: "confirm",
-            name: "proceed",
-            message: `Write configuration to ${chalk.green(
-              DEFAULT_CONFIG_FILENAME
-            )}. Proceed?`,
-            initial: true,
-          })
-
-          if (!proceed) {
-            process.exit(0)
-          }
-        }
-        logger.info("")
-        const spinner = ora(`Writing ${DEFAULT_CONFIG_FILENAME}...`).start()
-        const targetPath = path.resolve(cwd, DEFAULT_CONFIG_FILENAME)
-        await fs.writeFile(targetPath, JSON.stringify(config, null, 2), "utf8")
-        spinner.succeed()
-
-        await runInit(cwd, config)
+        await runInit(cwd, config, options.yes)
       }
 
       logger.info("")
@@ -107,45 +73,52 @@ export async function promptForConfig(cwd: string, skip = false) {
   const defaultConfig = defaultPullOptions
   const highlight = (text: string) => chalk.cyan(text)
 
-  const options = await prompts([
+  const options = await prompts(
+    [
+      {
+        type: "toggle",
+        name: "skipConversion",
+        message: `Would you to ${highlight(
+          "skip convertion to markdown"
+        )} (recommended)?`,
+        initial: defaultConfig.conversion.skip,
+        active: "yes",
+        inactive: "no",
+      },
+      {
+        type: "select",
+        name: "cacheStrategy",
+        message: `Which ${highlight("cache strategy")} would you like to use?`,
+        choices: cacheStrategiesSchema.options.map((cacheStrategy) => ({
+          title: cacheStrategy,
+          value: cacheStrategy,
+        })),
+      },
+      {
+        type: "select",
+        name: "rootObjectType",
+        message: `What is the ${highlight(
+          "root object type"
+        )} of your Notion content?`,
+        choices: rootObjectTypeSchema.options.map((type) => ({
+          title: type,
+          value: type,
+        })),
+      },
+      {
+        type: "text",
+        name: "rootId",
+        message: `What is the ${highlight("root ID")} of your Notion content?`,
+        initial: defaultConfig?.rootId ?? "",
+      },
+      // TODO: Complete the rest of the options
+    ],
     {
-      type: "toggle",
-      name: "skipConversion",
-      message: `Would you to ${highlight(
-        "skip convertion to markdown"
-      )} (recommended)?`,
-      initial: defaultConfig.conversion.skip,
-      active: "yes",
-      inactive: "no",
-    },
-    {
-      type: "select",
-      name: "cacheStrategy",
-      message: `Which ${highlight("cache strategy")} would you like to use?`,
-      choices: cacheStrategiesSchema.options.map((cacheStrategy) => ({
-        title: cacheStrategy,
-        value: cacheStrategy,
-      })),
-    },
-    {
-      type: "select",
-      name: "rootObjectType",
-      message: `What is the ${highlight(
-        "root object type"
-      )} of your Notion content?`,
-      choices: rootObjectTypeSchema.options.map((type) => ({
-        title: type,
-        value: type,
-      })),
-    },
-    {
-      type: "text",
-      name: "rootId",
-      message: `What is the ${highlight("root ID")} of your Notion content?`,
-      initial: defaultConfig?.rootId ?? "",
-    },
-    // TODO: Complete the rest of the options
-  ])
+      onCancel: () => {
+        process.exit(0)
+      },
+    }
+  )
 
   const config = configSchema.parse({
     // TODO: Set a schema $schema: "https://ui.shadcn.com/schema.json",
@@ -162,61 +135,28 @@ export async function promptForConfig(cwd: string, skip = false) {
   return await resolveConfigPaths(cwd, config)
 }
 
-export async function runInit(cwd: string, config: Config) {
-  const spinner = ora(`Initializing project...`)?.start()
+export async function runInit(
+  cwd: string,
+  config: Config,
+  skipConfirmation: boolean
+) {
+  if (!skipConfirmation) {
+    const { proceed } = await prompts({
+      type: "confirm",
+      name: "proceed",
+      message: `Write configuration to ${chalk.green(
+        DEFAULT_CONFIG_FILENAME
+      )}. Proceed?`,
+      initial: true,
+    })
 
-  spinner?.succeed()
-
-  // TODO: Implement init to create a config file. Consider spinner at the end
-  return
-
-  // Ensure all resolved paths directories exist.
-  for (const [key, resolvedPath] of Object.entries(config.resolvedPaths)) {
-    // Determine if the path is a file or directory.
-    // TODO: is there a better way to do this?
-    let dirname = path.extname(resolvedPath)
-      ? path.dirname(resolvedPath)
-      : resolvedPath
-
-    // If the utils alias is set to something like "@/lib/utils",
-    // assume this is a file and remove the "utils" file name.
-    // TODO: In future releases we should add support for individual utils.
-    if (key === "utils" && resolvedPath.endsWith("/utils")) {
-      // Remove /utils at the end.
-      dirname = dirname.replace(/\/utils$/, "")
-    }
-
-    if (!existsSync(dirname)) {
-      await fs.mkdir(dirname, { recursive: true })
+    if (!proceed) {
+      process.exit(0)
     }
   }
-
-  const extension = config.tsx ? "ts" : "js"
-
-  const tailwindConfigExtension = path.extname(
-    config.resolvedPaths.tailwindConfig
-  )
-
-  let tailwindConfigTemplate: string
-  if (tailwindConfigExtension === ".ts") {
-    tailwindConfigTemplate = config.tailwind.cssVariables
-      ? templates.TAILWIND_CONFIG_TS_WITH_VARIABLES
-      : templates.TAILWIND_CONFIG_TS
-  } else {
-    tailwindConfigTemplate = config.tailwind.cssVariables
-      ? templates.TAILWIND_CONFIG_WITH_VARIABLES
-      : templates.TAILWIND_CONFIG
-  }
-
-  // Write tailwind config.
-  await fs.writeFile(
-    config.resolvedPaths.tailwindConfig,
-    template(tailwindConfigTemplate)({
-      extension,
-      prefix: config.tailwind.prefix,
-    }),
-    "utf8"
-  )
-
-  spinner?.succeed()
+  logger.info("")
+  const spinner = ora(`Writing ${DEFAULT_CONFIG_FILENAME}...`).start()
+  const targetPath = path.resolve(cwd, DEFAULT_CONFIG_FILENAME)
+  await fs.writeFile(targetPath, JSON.stringify(config, null, 2), "utf8")
+  spinner.succeed()
 }
