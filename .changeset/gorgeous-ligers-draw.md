5a466d677b25503d11e9c90bc78ac9564c8b9de6
Francisco Moretti <franciscoemoretti@gmail.com>
2024-10-13 11:50:15 +0100

    package resolution

---
+++ .changeset/gorgeous-ligers-draw.md
@@ -0,0 +1,5 @@
+---
+"notion-downloader": patch
+---
+
+version reporting
--- packages/notion-downloader/src/utils/get-package-info.ts
+++ packages/notion-downloader/src/utils/get-package-info.ts
@@ -1,9 +1,12 @@
 import path from "path"
+import { fileURLToPath } from "url"
 import fs from "fs-extra"
 import { type PackageJson } from "type-fest"
 
 export function getPackageInfo() {
-  const packageJsonPath = path.join("package.json")
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  const packageJsonPath = path.resolve(__dirname, "..", "package.json")
 
   return fs.readJSONSync(packageJsonPath) as PackageJson
 }
