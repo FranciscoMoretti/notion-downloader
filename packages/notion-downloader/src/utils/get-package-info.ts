32410b0a2a0cd9b893d3933b6b61b950e6a3b458
Francisco Moretti <franciscoemoretti@gmail.com>
2024-10-14 07:50:53 +0100

    bugfix dirname cjs

--- packages/notion-downloader/src/utils/get-package-info.ts
+++ packages/notion-downloader/src/utils/get-package-info.ts
@@ -4,9 +4,16 @@ import fs from "fs-extra"
 import { type PackageJson } from "type-fest"
 
 export function getPackageInfo() {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
-  const packageJsonPath = path.resolve(__dirname, "..", "package.json")
+  let packageJsonPath: string
+  if (typeof __dirname !== "undefined") {
+    // CommonJS
+    packageJsonPath = path.resolve(__dirname, "..", "package.json")
+  } else {
+    // ESM
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    packageJsonPath = path.resolve(__dirname, "..", "..", "package.json")
+  }
 
   return fs.readJSONSync(packageJsonPath) as PackageJson
 }
