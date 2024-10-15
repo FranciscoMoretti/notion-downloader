1b11362e7246cad355185f9e6956d41e6f7594eb
Francisco Moretti <franciscoemoretti@gmail.com>
2024-10-15 09:56:10 +0100

    bugfix double url encoding

--- packages/notion-downloader/src/files/markdownPathUtils.ts
+++ packages/notion-downloader/src/files/markdownPathUtils.ts
@@ -1,4 +1,9 @@
 export function convertMarkdownPath(path: string): string {
   const posixPath = path.replace(/\\/g, "/")
-  return encodeURI(posixPath)
+  // TODO: Ensure it's only encoded once by figuring out where the naming strategy gets applied in FilesManager get method
+  // IF not already url encoded, encode it
+  if (!posixPath.includes("%")) {
+    return encodeURI(posixPath)
+  }
+  return posixPath
 }
